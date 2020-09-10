f76f6186a13e791fe512e53fcd29aac68bf11f5b
Varun Malhotra <12003362+smartgigs94@users.noreply.github.com>
2020-09-10 11:17:50 +0530

    details file added

---
+++ details.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+#This script is to check system variables and other important resouces
+
+#shell name
+echo "Shell: $SHELL"
+#kernel version
+echo "Kernel Version: `uname -r`"
+#hostname
+echo "Hostname: `hostname`"
+#Home Directory
+echo "Home: $HOME"
+
+
+
